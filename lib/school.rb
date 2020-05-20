5e8983316eb4de8a242d248e97810cd3fea5426b
Megan Gomes <megan.gomes25@gmail.com>
2020-05-20 23:56:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
     @roster
   end  
   
-  def add_student(student_name, grade)
+  def add_student(name, grade)
     @student_name = name
     @grade = grade
     if roster.include?(grade) == false
