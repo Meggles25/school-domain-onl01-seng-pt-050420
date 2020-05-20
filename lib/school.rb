70929abb0d222f2c8e6140b7bcba967576a16ca4
Megan Gomes <megan.gomes25@gmail.com>
2020-05-20 23:52:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+class School
+
+  attr_accessor :name, :roster
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def roster
+    @roster = roster
+  end  
+  
+  def add_student(student_name, grade)
+    @student_name = name
+    @grade = grade
+    if roster.include(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+  
+  def grade(num)
+    @roster[num]
+  end
+  
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+end  
+
+  
+  
+  
